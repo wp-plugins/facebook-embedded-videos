b197bba7b6bcba1407d3e7051265947569539bcd
bigbluewp <bigbluewp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 03:07:30 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/facebook-embedded-videos/trunk@1152557 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebook_embedded_videos.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * @package facebook_embedded_videos
+*/
+/*
+Plugin Name: Facebook Embedded Videos
+Plugin URI: http://bigbluegroup.net/
+Description: With the Embedded Video Player you can easily add Facebook Videos to your website. You can use any public video post by a page or a person as video source.
+Version: 0.1
+Author: Big Blue Group
+Author URI: http://bigbluegroup.net/
+*/
+
+class FacebookEmbeddedVideos extends WP_Widget{
+    
+    public function __construct() {
+        $params = array(
+            'description' => 'With the Embedded Video Player you can easily add Facebook Videos to your website. You can use any public video post by a page or a person as video source.',
+            'name' => 'Facebook Embedded Videos '
+        );
+        parent::__construct('FacebookEmbeddedVideos','',$params);
+    }
+    
+    public function form($instance) {
+        extract($instance);
+        
+        ?>
+                   <!-- Color Picker Script Start -->
+
+<script type='text/javascript'>
+    jQuery(document).ready(function($) {
+        $('.my-color-picker').wpColorPicker();
+    });
+</script>
+
+<!-- Color Picker Script End -->
+
+<!-- here will put all widget configuration -->
+<p>
+    <label for="<?php echo $this->get_field_id('title');?>">Title : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('title');?>"
+	name="<?php echo $this->get_field_name('title');?>"
+        value="<?php echo !empty($title) ? $title : "Facebook Embedded Posts"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('moduleclass_sfx');?>">Module Suffix : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('moduleclass_sfx');?>"
+	name="<?php echo $this->get_field_name('moduleclass_sfx');?>"
+        value="<?php echo !empty($moduleclass_sfx) ? $moduleclass_sfx : " "; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('fb_url');?>">URL of post : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('fb_url');?>"
+	name="<?php echo $this->get_field_name('fb_url');?>"
+        value="<?php echo !empty($fb_url) ? $fb_url : "https://www.facebook.com/video.php?v=10152454700553553"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('width');?>">Width : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('width');?>"
+	name="<?php echo $this->get_field_name('width');?>"
+        value="<?php echo !empty($width) ? $width : "300"; ?>" />
+</p>
+
+<p>
+    <label for="<?php echo $this->get_field_id('background');?>">Background Color: </label><br>
+    <input
+    class="my-color-picker"
+    id="<?php echo $this->get_field_id('background');?>"
+    name="<?php echo $this->get_field_name('background');?>"
+        value="<?php echo !empty($background) ? $background : "#FDFDFD"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('border');?>">Border Color: </label><br>
+    <input
+    class="my-color-picker"
+    id="<?php echo $this->get_field_id('border');?>"
+    name="<?php echo $this->get_field_name('border');?>"
+        value="<?php echo !empty($border) ? $border : "#F5E6E6"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('outline');?>">Outline Color: </label><br>
+    <input
+    class="my-color-picker"
+    id="<?php echo $this->get_field_id('outline');?>"
+    name="<?php echo $this->get_field_name('outline');?>"
+        value="<?php echo !empty($outline) ? $outline : "#FFFDFD"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('radius');?>">Border Radius : </label>
+    <input
+    class="widefat"
+    id="<?php echo $this->get_field_id('radius');?>"
+    name="<?php echo $this->get_field_name('radius');?>"
+        value="<?php echo !empty($radius) ? $radius : "5"; ?>" />
+</p>
+
+<?php
+    }
+    
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+	    if(empty($title)) $title = "Facebook Embedded Posts ";
+	    if(empty($moduleclass_sfx)) $moduleclass_sfx = "  ";
+        if(empty($fb_url)) $fb_url = "https://www.facebook.com/video.php?v=10152454700553553";
+        if(empty($width)) $width = "300";
+        if(empty($background)) $background = "#FDFDFD";
+        if(empty($outline)) $outline = "#FFFDFD";
+        if(empty($radius)) $radius = "5px";
+        if(empty($border)) $border = "#F5E6E6";
+        echo $before_widget;
+            echo $before_title . $title . $after_title;
+            
+            ?>
+   <style type="text/css">
+    #fb-root {
+        display: none;
+    }
+
+    .fb_iframe_widget, .fb_iframe_widget span, .fb_iframe_widget span iframe[style] {
+        width: 100% !important;
+    }   
+    .pluginSkinLight.pluginFontHelvetica > div >* {
+      width: 100% !important;
+    }
+
+    .facebook_embedded_videos{
+        padding:5px;
+        border: 1px solid <?php echo $border;?>;
+        outline: 5px solid <?php echo $outline;?>;
+        background: <?php echo $background;?>;
+        border-radius: <?php echo $radius;?>px;
+    }
+
+</style>      
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=262562957268319&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+ <div class="facebook_embedded_videos <?php echo $moduleclass_sfx;?>">
+           <div class="fb-video"
+              data-href="<?php echo $fb_url;?>"
+              data-width="<?php echo $width;?>">
+            </div>
+ </div>
+<?php
+        echo $after_widget;
+    }
+}
+
+//registering the color picker
+add_action( 'load-widgets.php', 'my_custom_load_video' );
+
+function my_custom_load_video() {    
+    wp_enqueue_style( 'wp-color-picker' );        
+    wp_enqueue_script( 'wp-color-picker' );    
+}
+
+
+add_action('widgets_init','register_FacebookEmbeddedVideos');
+function register_FacebookEmbeddedVideos(){
+    register_widget('FacebookEmbeddedVideos');
+}
---
+++ readme.txt
@@ -0,0 +1,62 @@
+﻿=== Facebook Embedded Videos ===
+
+Plugin URI: http://bigbluegroup.net/
+Author Name : Big Blue Group
+Author URL : http://bigbluegroup.net/
+Tags: Facebook videos, facebook embedd videos, facebook embedded videos widget, wordpress widget
+Requires at least: 3.0.1
+Tested up to: 4.2.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+With the Embedded Video Player you can easily add Facebook Videos to your website. You can use any public video post by a page or a person as video source.
+
+== Description ==
+
+Play videos from facebook now easy.
+
+Can edit border color, border radius, outline color from backend along with module width.
+
+Completely Fluid.
+
+100% Free
+
+== Installation ==
+
+Login to your wordpress admin - example : http://www.domain.com/wp-admin
+go to "Plugins->Installed Plugins" from Menu
+Click "Add New" - button at top
+Click "Upload" - from the list
+Click "Choose File" - button
+a windows will open of your computer - browse file - select the package
+click "Install Now"
+
+On successful completion of installation you will get a success massage.
+
+Navigate to - "Widget" Page.
+
+Note: Embedded Posts are a simple way to put public posts - by a Page or a person on Facebook - into the content of your web site or web page. Only public posts from Facebook Pages and profiles can be embedded.
+
+Get Video Post URL
+
+First you need to get the URL of a video post you wish to share. The video post must be public, which is indicated by the gray world icon, right next the post's publishing time:  
+
+For testing you can use this example URL:
+
+"https://www.facebook.com/FacebookDevelopers/videos/10152454700553553/"
+
+== Frequently Asked Questions ==
+
+Feel free to contact us anytime
+
+Contact Email :- bigbluegroup@outlook.com
+
+== Screenshots ==
+
+1. Backend Screenshot
+2. Frontend Screenshot
+
+== Changelog ==
+
+= 0.1 =
+Stable version release
